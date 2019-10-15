1793c643d7cec07bf5c9d9b3f78d8d774f3c87e0
fluke-jesadakorn <jesadakorn.kirtnu@gmail.com>
2019-10-15 23:09:04 +0700

    example router

---
+++ src/component/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                this is Home Pages
+            </div>
+        )
+    }
+}
